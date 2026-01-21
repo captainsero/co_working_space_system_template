6a6467f922bb9029c77ee8cb40d89790dd34bd1b
Yasser Samir <yasoo976098@gmail.com>
2026-01-21 18:35:57 +0200

    Refactor code structure for improved readability and maintainability

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -1,4 +1,4 @@
 class Images {
-  static String teamIcon = "assets/team_icon.jpg";
+  static String teamIcon = "assets/team_icon.png";
   static String tWithoutBackground = "assets/t_without_icon.png";
 }
